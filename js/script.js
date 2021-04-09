246a8b2f1e559803bb3774e6db2fb7f301dada54
Dejan <dejan.bojovic88@gmail.com>
2021-04-09 23:00:36 +0200

    check

--- js/script.js
+++ js/script.js
@@ -6,11 +6,6 @@ homepage.style.display = 'none'
 
 btn.addEventListener('click', () => {
 
-    // setTimeout(() => {
-    //     btn.style.backgroundColor = 'pink'
-    //     btn.style.color = 'purple'
-    // }, 2000)
-
     setTimeout(() => {
         frontpage.style.display = "none"
         homepage.style.display = ''
