d23f308471b823ef486613038e34fb1003e0fc67
Dejan <dejan.bojovic88@gmail.com>
2021-04-12 01:45:36 +0200

    working on homepage

--- js/script.js
+++ js/script.js
@@ -22,7 +22,6 @@ sessionStorage.setItem('frontpage', 'false')
 // toggle and search section
 const searchInput = document.querySelector("input[type='search']")
 console.log(searchInput)
-const searchButton = document.querySelector('.search-btn')
 const toggle = document.querySelector('.search-toggle')
 const toggleThumb = document.querySelector('.toggle-thumb')
 
@@ -43,17 +42,81 @@ toggle.addEventListener('click', () => {
     }
 })
 
+const mealCreation = (url, title) => {
+    return `
+        <div class="meal">
+            <img src="${url}" alt="">
+            <h2>${title}</h2>
+        </div>
+    `
+}
+
+// api call for meal
+const searchButton = document.querySelector('.search-btn')
+const resultContainer = document.querySelector('.results')
+
+searchButton.addEventListener('click', () => {
+    const searchStr = searchInput.value
+    console.log(searchStr)
+
+    const apiKey = '8728ac3aa56f4c2ea352d5fe0de10fbf'
+
+    if(searchInput.placeholder === 'Main ingredient') {
+        fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${searchStr}&apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            // for(let i = 0; i < data.length; i++) {
+            //     let mrs = mealCreation(data[i].image, data[i].title)
+            //     resultContainer.insertAdjacentHTML('afterbegin', mrs)
+            // }
+            data.map(el => {
+                console.log(el.title)
+                console.log(el)
+                resultContainer.insertAdjacentHTML('afterbegin', mealCreation(el.image, el.title))
+            });
+
+            // adding a red dot so user knows what happens when he likes a meal
+            const allMeals = document.querySelectorAll('.meal')
+            allMeals.forEach(el => {
+                el.addEventListener('click', () => {
+                    document.querySelector('.favorites-dot').style.display = "block"
+                    // setTimeout(() => {
+                    //     document.querySelector('.favorites-dot').style.display = "none"
+                    // }, 2000)
+                })
+            })
+        })
+    } else if(searchInput.placeholder === 'Dish name') {
+        fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=eggplant&apiKey=${apiKey}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
+})
+
 
 // menu
 const homeButton = document.querySelector('.home')
 const favoritesButton = document.querySelector('.favorites')
 
 homeButton.addEventListener('click', () => {
-    homeButton.style.color = '#212121'
-    favoritesButton.style.color = '#fff'
+    homeButton.style.color = '#000'
+    homeButton.style.backgroundColor = '#fcd06a'
+
+    favoritesButton.style.color = '#686868'
+    favoritesButton.style.backgroundColor = 'transparent'
 })
 
 favoritesButton.addEventListener('click', () => {
-    homeButton.style.color = '#fff'
-    favoritesButton.style.color = '#212121'
+    // removing the red dot
+    document.querySelector('.favorites-dot').style.display = "none"
+
+    favoritesButton.style.color = '#000'
+    favoritesButton.style.backgroundColor = '#fcd06a'
+    favoritesButton.style.borderRadius = '10px'
+
+    homeButton.style.color = '#686868'
+    homeButton.style.backgroundColor = 'transparent'
 })
